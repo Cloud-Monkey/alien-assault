5adab1e674f27eea29ee66dbe72e5033dece4331
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-08-12 13:12:23 +0000

    add shooter div and basic style

--- app.js
+++ app.js
@@ -20,4 +20,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // draw the alien invaders
     alienInvaders.forEach( invader => squares[currentInvaderIndex + invader].classList.add('invader'))
+
+    // draw the shooter
+    squares[currentShooterIndex].classList.add('shooter')
+    console.log(squares[202])
 })
