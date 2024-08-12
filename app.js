fc8438b7cb154b51534e6bec94557173278db0f7
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-08-12 10:07:53 +0000

    format app.js file

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ document.addEventListener("DOMContentLoaded", () => {
     let direction = 1
     let invaderId
 
-// define the alien invaders
+    // define the alien invaders
     const alienInvaders = [
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39
     ]
 
-// draw the alien invaders
-    alienInvaders.forEach( invader => squares[currentInvaderIndex + invader].classList.add('invader'))
+    // draw the alien invaders
+    alienInvaders.forEach(invader => squares[currentInvaderIndex + invader].classList.add('invader'))
 })
