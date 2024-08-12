e4fe904be22923efe6a33906a792edac38f9d7ea
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-08-12 13:37:30 +0000

    add move shooter function and event listner for keydown on the left and right arrow keys

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 document.addEventListener("DOMContentLoaded", () => {
     const squares = document.querySelectorAll('.grid div')
     const resultDisplay = document.querySelector('#result')
@@ -13,15 +11,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // define the alien invaders
     const alienInvaders = [
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
-        15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
         30, 31, 32, 33, 34, 35, 36, 37, 38, 39
     ]
 
     // draw the alien invaders
-    alienInvaders.forEach( invader => squares[currentInvaderIndex + invader].classList.add('invader'))
+    alienInvaders.forEach(invader => squares[currentInvaderIndex + invader].classList.add('invader'))
 
     // draw the shooter
     squares[currentShooterIndex].classList.add('shooter')
-    console.log(squares[202])
+
+    // move the shooter along a line
+    function moveShooter(e) {
+        squares[currentShooterIndex].classList.remove('shooter')
+        switch (e.keyCode) {
+            case 37:
+                if (currentShooterIndex % width !== 0) currentShooterIndex -= 1
+                break
+            case 39:
+                if (currentShooterIndex % width < width - 1) currentShooterIndex += 1
+                break
+        }
+        squares[currentShooterIndex].classList.add('shooter')
+    }
+
+    document.addEventListener('keydown', moveShooter)
+
 })
