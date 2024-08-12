994c3e5890f31623c087dbc97ebbbcccdd288be9
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-08-12 10:30:52 +0000

    edit array bug

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-    const squares = document.querySelectorAll(".grid div")
-    const resultDisplay = document.querySelector("#result")
+    const squares = document.querySelectorAll('.grid div')
+    const resultDisplay = document.querySelector('#result')
     let width = 15
     let currentShooterIndex = 202
     let currentInvaderIndex = 0
-    let alienInvadersShotDown = []
+    let alienInvadersTakenDown = []
     let result = 0
     let direction = 1
     let invaderId
 
     // define the alien invaders
     const alienInvaders = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
+        15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 
+        30, 31, 32, 33, 34, 35, 36, 37, 38, 39
     ]
 
     // draw the alien invaders
     alienInvaders.forEach(invader => squares[currentInvaderIndex + invader].classList.add('invader'))
+    // console.log(squares[1])
 })
