e9503bf714dba71a62725f2e8bac8b480fa20652
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-08-13 15:30:39 +0000

    change grid to 20x20 and added more invaders

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const squares = document.querySelectorAll('.grid div')
     const resultDisplay = document.querySelector('#result')
-    let width = 15
-    let currentShooterIndex = 202
+    let width = 20
+    let currentShooterIndex = 350
     let currentInvaderIndex = 0
     let alienInvadersTakenDown = []
     let result = 0
@@ -14,9 +14,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // define the alien invaders
     const alienInvaders = [
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-        15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-        30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+         0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
+        20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+        60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70
     ]
 
     // draw the alien invaders
