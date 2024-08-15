6f1f1b5b654f042da423caf6b53e756df2470dcc
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-08-15 10:44:12 +0000

    add audio controls index page

--- app.js
+++ app.js
@@ -1,127 +1,63 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const squares = document.querySelectorAll('.grid div')
-    const resultDisplay = document.querySelector('#result')
-    let width = 20
-    let currentShooterIndex = 390
-    let currentInvaderIndex = 0
-    let alienInvadersTakenDown = []
-    let result = 0
-    let direction = 1
-    let invaderId
-    
-    // play game music on load
-    let titlemusic = document.getElementsByClassName("title-music");
-    titlemusic.volume = 0.1;
+const audio = document.getElementById("title-music");
+const playpause = document.getElementById("play-pause");
+const volume = document.getElementById("volume");
+const mutebtn = document.getElementById("mute");
+const controls = document.querySelector(".controls");
 
-    // define the alien invaders
-    const alienInvaders = [
-         0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
-        20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-        40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-        60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70
-    ]
-
-    // draw the alien invaders
-    alienInvaders.forEach(invader => squares[currentInvaderIndex + invader].classList.add('invader'))
-
-    // draw the shooter
-    squares[currentShooterIndex].classList.add('shooter')
-
-    // move the shooter along a line
-    function moveShooter(e) {
-        squares[currentShooterIndex].classList.remove('shooter')
-        switch (e.keyCode) {
-            case 37:
-                if (currentShooterIndex % width !== 0) currentShooterIndex -= 1
-                break
-            case 39:
-                if (currentShooterIndex % width < width - 1) currentShooterIndex += 1
-                break
-        }
-        squares[currentShooterIndex].classList.add('shooter')
+// Play-Pause
+playpause.addEventListener("click", function () {
+    if (audio.paused) {
+        audio.play();
+        playpause.innerHTML = '<i class="fa-solid fa-pause"></i>';
+    } else {
+        audio.pause();
+        playpause.innerHTML = '<i class="fa-solid fa-play"></i>';
     }
+});
 
-    document.addEventListener('keydown', moveShooter)
+let isPlaying = false;
 
-    // moves the alien invaders
-    function moveInvaders() {
-        const leftEdge = alienInvaders[0] % width === 0
-        const rightEdge = alienInvaders[alienInvaders.length - 1] % width === width - 1
-
-        if ((leftEdge && direction === -1) || (rightEdge && direction === 1)) {
-            direction = width
-        } else if (direction === width) {
-            if (leftEdge) direction = 1
-            else direction = -1
-        }
-        for (let i = 0; i <= alienInvaders.length - 1; i++) {
-            squares[alienInvaders[i]].classList.remove('invader')
-        }
-        for (let i = 0; i <= alienInvaders.length - 1; i++) {
-            alienInvaders[i] += direction
-        }
-        for (let i = 0; i <= alienInvaders.length - 1; i++) {
-            if (!alienInvadersTakenDown.includes(i)) {
-                squares[alienInvaders[i]].classList.add('invader')
-            }
-        }
-
-        // decide a game is over
-        if (squares[currentShooterIndex].classList.contains('invader', 'shooter')) {
-            resultDisplay.textContent = 'Game Over';
-            squares[currentShooterIndex].classList.add('boom')
-            clearInterval(invaderId)
-        }
-
-        for (let i = 0; i <= alienInvaders.length - 1; i++) {
-            if (alienInvaders[i] > (squares.length - (width - 1))) {
-                resultDisplay.textContent = 'Game Over';
-                clearInterval(invaderId)
-            }
-        }
-        //  decide a win
-        if(alienInvadersTakenDown.length === alienInvaders.length) {
-            resultDisplay.textContent = 'You Win!'
-            clearInterval(invaderId)
-        }
+// Function to toggle play/pause
+function togglePlayPause() {
+    if (isPlaying) {
+        audio.pause();
+        playpause.innerHTML = '<i class="fa-solid fa-play"></i>';
+    } else {
+        audio.play();
+        playpause.innerHTML = '<i class="fa-solid fa-pause"></i>';
     }
-    invaderId = setInterval(moveInvaders, 500)
+    isPlaying = !isPlaying;
+}
 
-    // shoot at aliens
-    function shoot(e) {
-        let laserId
-        let currentLaserIndex = currentShooterIndex
-        // move laser to the alien from the shooter
-        function moveLaser() {
-            squares[currentLaserIndex].classList.remove('laser')
-            currentLaserIndex -= width
-            squares[currentLaserIndex].classList.add('laser')
-            if (squares[currentLaserIndex].classList.contains('invader')) {
-                squares[currentLaserIndex].classList.remove('laser')
-                squares[currentLaserIndex].classList.remove('invader')
-                squares[currentLaserIndex].classList.add('boom')
+// Event listener for the audio to
+// update the isPlaying flag
+audio.addEventListener("play", function () {
+    isPlaying = true;
+});
 
-                setTimeout(() => squares[currentLaserIndex].classList.remove('boom'), 250)
-                clearInterval(laserId)
+audio.addEventListener("pause", function () {
+    isPlaying = false;
+});
 
-                const alienTakenDown = alienInvaders.indexOf(currentLaserIndex)
-                alienInvadersTakenDown.push(alienTakenDown)
-                result += 100
-                resultDisplay.textContent = result
-            }
-
-            if (currentLaserIndex < width) {
-                clearInterval(laserId)
-                setTimeout(() => squares[currentLaserIndex].classList.remove('laser'), 100)
-            }
-        }
-
-        switch(e.keyCode) {
-            case 32:
-                laserId = setInterval(moveLaser, 100)
-                break
-        }
+// Mute or Unmute
+mutebtn.addEventListener("click", function () {
+    if (audio.muted) {
+        audio.muted = false;
+        mutebtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+        volume.value = audio.volume;
+    } else {
+        audio.muted = true;
+        mutebtn.innerHTML = '<i class="fa-solid fa-volume-xmark"></i>';
+        volume.value = 0;
     }
+});
 
-    document.addEventListener('keyup', shoot)
-})
+// adjust mute icon on volume change
+volume.addEventListener("input", function () {
+    audio.volume = volume.value;
+    if (audio.volume === 0) {
+        mutebtn.innerHTML = '<i class="fa-solid fa-volume-xmark"></i>';
+    } else {
+        mutebtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+    }
+});
